99c27d0d16c4bdb5e56aa8463afffaeb10b27f48
hakkiduman <ihduman94@gmail.com>
2020-11-21 20:58:07 +0300

    section 3 end

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,12 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
                 });
+                
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy=> {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -42,7 +48,10 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
-
+            app.UseCors("CorsPolicy");
+            // app.UseEndpoints(endpoints => {
+            //     endpoints.MapControllers();
+            // });
             app.UseRouting();
 
             app.UseAuthorization();
